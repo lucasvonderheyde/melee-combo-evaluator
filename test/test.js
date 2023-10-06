6aa2d4b7411f6b293c923098c435b2c862e6ecdc
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-10-06 19:45:34 -0400

    added organaztion and initlization of json folders

--- test/test.js
+++ test/test.js
@@ -1,20 +1,29 @@
 const { SlippiGame } = require("@slippi/slippi-js");
 const fs = require("fs");
 
-const game = new SlippiGame("test.slp");
+const game = new SlippiGame("./slp_files/Stream-Game_20220828T205441.slp");
+
+// Generate a unique folder name using a timestamp (Added)
+const outputDir = `./jsondb/output_folder_${Date.now()}`;  // (Added)
+
+// Create the new folder (Added)
+if (!fs.existsSync(outputDir)){ // (Added)
+    fs.mkdirSync(outputDir); // (Added)
+}
 
 // Get game settings – stage, characters, etc
 const settings = game.getSettings();
-const settingsFilePath = "settings.json";
+const settingsFilePath = `${outputDir}/settings.json`; // (Modified)
 fs.writeFileSync(settingsFilePath, JSON.stringify(settings));
 console.log(`Settings saved to ${settingsFilePath}`);
 
-lowerPortPlayer = settings.players[0].playerIndex
-higherPortPlayer =settings.players[1].playerIndex
+// Your existing variable declarations here...
+lowerPortPlayer = settings.players[0].playerIndex;
+higherPortPlayer = settings.players[1].playerIndex;
 
 // Get metadata - start time, platform played on, etc
 const metadata = game.getMetadata();
-const metadataFilePath = "metadata.json";
+const metadataFilePath = `${outputDir}/metadata.json`; // (Modified)
 fs.writeFileSync(metadataFilePath, JSON.stringify(metadata));
 console.log(`Metadata saved to ${metadataFilePath}`);
 
@@ -42,6 +51,6 @@ while (frameIndex < lastFrame) {
 }
 
 // Specify the output file where all "post" frame data will be saved
-const postFrameDataFilePath = "all_post_frames.json";
+const postFrameDataFilePath = `${outputDir}/all_post_frames.json`; // (Modified)
 fs.writeFileSync(postFrameDataFilePath, JSON.stringify(allPostFrames));
 console.log(`All "post" frame data saved to ${postFrameDataFilePath}`);
