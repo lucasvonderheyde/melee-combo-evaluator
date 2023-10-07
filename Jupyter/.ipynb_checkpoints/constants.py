f807905195aa3e6b9980cb939a2c5de403d46eae
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-10-07 01:21:24 -0400

    cleaned up more data tables

---
+++ Jupyter/.ipynb_checkpoints/constants.py
@@ -0,0 +1,17 @@
+internal_character_ids = {
+    0: "Mario",
+    1: "Fox",
+    7: "Shiek",
+    18: "Marth",
+    19: "Zelda"
+}
+
+internal_character_name_lookup = {v: k for k, v in internal_character_ids.items()}
+
+settings_character_id = {
+
+}
+
+stage_ids ={
+
+}
