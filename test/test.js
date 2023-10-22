f400b5bfd0f37c21ff55ca15787aa6c55776f93a
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-10-21 23:23:28 -0400

    cleaned up classes began the process of adding the preframe data

--- test/test.js
+++ test/test.js
@@ -3,54 +3,52 @@ const fs = require("fs");
 
 const game = new SlippiGame("./slp_files/Game_20230927T210616.slp");
 
-// Generate a unique folder name using a timestamp (Added)
-const outputDir = `./jsondb/output_folder_${Date.now()}`;  // (Added)
+const outputDir = `./jsondb/output_folder_${Date.now()}`;
 
-// Create the new folder (Added)
-if (!fs.existsSync(outputDir)){ // (Added)
-    fs.mkdirSync(outputDir); // (Added)
+if (!fs.existsSync(outputDir)){
+    fs.mkdirSync(outputDir);
 }
 
-// Get game settings – stage, characters, etc
 const settings = game.getSettings();
-const settingsFilePath = `${outputDir}/settings.json`; // (Modified)
+const settingsFilePath = `${outputDir}/settings.json`;
 fs.writeFileSync(settingsFilePath, JSON.stringify(settings));
 console.log(`Settings saved to ${settingsFilePath}`);
 
-// Your existing variable declarations here...
-lowerPortPlayer = settings.players[0].playerIndex;
-higherPortPlayer = settings.players[1].playerIndex;
+const lowerPortPlayer = settings.players[0].playerIndex;
+const higherPortPlayer = settings.players[1].playerIndex;
 
-// Get metadata - start time, platform played on, etc
 const metadata = game.getMetadata();
-const metadataFilePath = `${outputDir}/metadata.json`; // (Modified)
+const metadataFilePath = `${outputDir}/metadata.json`;
 fs.writeFileSync(metadataFilePath, JSON.stringify(metadata));
 console.log(`Metadata saved to ${metadataFilePath}`);
 
-// Extract the last frame from the metadata
 const lastFrame = metadata.lastFrame;
-
-// Get frames – animation state, inputs, etc
 const frames = game.getFrames();
 
 const allPostFrames = [];
+const allPreFrames = [];  // Added this line to store "pre" frames
 
 let frameIndex = 0;
 
 while (frameIndex < lastFrame) {
     const frame = frames[frameIndex];
     
-    // Extract "post" frames for player 2 and player 3
     const lowerPortPlayerPostFrame = frame.players[lowerPortPlayer].post;
     const higherPortPlayerPostFrame = frame.players[higherPortPlayer].post;
 
-    // Push the "post" frames to the array
-    allPostFrames.push({ lowerPortPlayerPostFrame: lowerPortPlayerPostFrame, higherPortPlayerPostFrame: higherPortPlayerPostFrame });
+    const lowerPortPlayerPreFrame = frame.players[lowerPortPlayer].pre;  // Added this line
+    const higherPortPlayerPreFrame = frame.players[higherPortPlayer].pre;  // Added this line
+
+    allPostFrames.push({ lowerPortPlayerPostFrame, higherPortPlayerPostFrame });
+    allPreFrames.push({ lowerPortPlayerPreFrame, higherPortPlayerPreFrame });  // Added this line
 
     frameIndex++;
 }
 
-// Specify the output file where all "post" frame data will be saved
-const postFrameDataFilePath = `${outputDir}/all_post_frames.json`; // (Modified)
+const postFrameDataFilePath = `${outputDir}/all_post_frames.json`;
 fs.writeFileSync(postFrameDataFilePath, JSON.stringify(allPostFrames));
 console.log(`All "post" frame data saved to ${postFrameDataFilePath}`);
+
+const preFrameDataFilePath = `${outputDir}/all_pre_frames.json`;  // Added this line
+fs.writeFileSync(preFrameDataFilePath, JSON.stringify(allPreFrames));  // Added this line
+console.log(`All "pre" frame data saved to ${preFrameDataFilePath}`);  // Added this line
