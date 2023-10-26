7920f3420d44f82a322aeaed60cae0ce3d792648
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-10-26 00:44:04 -0400

    testing data

--- src/gather_combo.py
+++ src/gather_combo.py
@@ -3,3 +3,15 @@ from sqlalchemy import create_engine
 from database_info import database
 
 engine = create_engine(database)
+
+
+
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_columns', 60)
+
+query = "SELECT * FROM higher_port_player_post_frames LEFT JOIN higher_port_player_pre_frames ON higher_port_player_post_frames.frame = higher_port_player_pre_frames.frame"
+
+dataframe = pd.read_sql(query, engine)
+
+print(dataframe)
--- test/original_script.js
+++ test/original_script.js
@@ -8,20 +8,21 @@ console.log(settings.players[1].playerIndex);
 
 // Get metadata - start time, platform played on, etc
 const metadata = game.getMetadata();
-// console.log(metadata);
+console.log(metadata);
 
 // Get computed stats - openings / kill, conversions, etc
 const stats = game.getStats();
-// console.log(stats);
+console.log(stats);
 
 // Get frames – animation state, inputs, etc
 // This is used to compute your own stats or get more frame-specific info (advanced)
 const frames = game.getFrames();
 console.log(frames[0].players); // Print frame when timer starts counting downs
 
-let frame = 0;
 
-while (frame < 12034){
-    console.log(frames[frame].players)
-    frame++
-}
+// let frame = 0;
+
+// while (frame < 12034){
+//     console.log(frames[frame].players)
+//     frame++
+// }
