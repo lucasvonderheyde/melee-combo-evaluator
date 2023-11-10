99fd6c4bdadb0bdb58858c22131dfd851ed2ac48
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-11-10 13:12:47 -0500

    gathering combos and labeling now works with multiple game included

--- src/label_combos_for_model.py
+++ src/label_combos_for_model.py
@@ -6,7 +6,7 @@ from src.database_info import database
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-game_id = '3446534c-ed7f-496c-a8b5-ca346b3d65ba'
+game_id = 'ce32f9f5-f640-4844-a4e9-ac932cd679ab'
 
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.width', 1000)
@@ -17,10 +17,8 @@ pd.set_option('display.max_rows', 1000)
 engine = create_engine(database)
 
 
-query = 'SELECT * FROM combos_for_battlefield WHERE higher_post_game_id = %s'
-query_2 = '''SELECT * FROM combos_for_battlefield WHERE higher_post_game_id = %s'''
+query = '''SELECT * FROM combos_for_battlefield WHERE higher_post_game_id = %s'''
 df = pd.read_sql(query, engine, params=(game_id,))
-df_2 = pd.read_sql(query_2, engine, params=(game_id,))
 
 battlefield_x_axis = 228
 battlefield_y_axis_top_blastzone = 208
@@ -39,25 +37,25 @@ successful_l_cancel = 1
 unsuccessful_l_cancel = 0
 no_l_cancel_on_frame = None
 
-df_2['combo_block_for_model'] = None
-df_2['character_creating_combo_for_model'] = None
-df_2['humanlabel'] = None
-df_2['attack_state_to_hit_in_combo_for_model'] =  None
-df_2['lower_port_l_cancel_for_model'] = None
-df_2['higher_port_l_cancel_for_model'] = None
-df_2['lower_port_damage_done_with_combo'] = None
-df_2['higher_port_damage_done_with_combo'] = None
-df_2['lower_port_damage_done_with_combo_model_score'] = None
-df_2['higher_port_damage_done_with_combo_model_score'] = None
-df_2['higher_port_x_position_model_score'] = None
-df_2['higher_port_y_position_model_score'] = None
-df_2['lower_port_x_position_model_score'] = None
-df_2['lower_port_y_position_model_score'] = None
-df_2['higher_death_state_blocks'] = None
-df_2['lower_death_state_blocks'] = None
+df['combo_block_for_model'] = None
+df['character_creating_combo_for_model'] = None
+df['humanlabel'] = None
+df['attack_state_to_hit_in_combo_for_model'] =  None
+df['lower_port_l_cancel_for_model'] = None
+df['higher_port_l_cancel_for_model'] = None
+df['lower_port_damage_done_with_combo'] = None
+df['higher_port_damage_done_with_combo'] = None
+df['lower_port_damage_done_with_combo_model_score'] = None
+df['higher_port_damage_done_with_combo_model_score'] = None
+df['higher_port_x_position_model_score'] = None
+df['higher_port_y_position_model_score'] = None
+df['lower_port_x_position_model_score'] = None
+df['lower_port_y_position_model_score'] = None
+df['higher_death_state_blocks'] = None
+df['lower_death_state_blocks'] = None
 
 
-for index, row in df_2.iterrows():
+for index, row in df.iterrows():
     current_frame = row['higher_post_frame'] 
 
     # Check for frame gaps to identify new combos
@@ -66,23 +64,23 @@ for index, row in df_2.iterrows():
 
     # Label based on action_state_id
     if row["lower_post_action_state_id"] not in action_state_id_to_check_getting_hit:#lower port player creating combo
-        df_2.at[index, 'combo_block_for_model'] = current_combo_id 
-        df_2.at[index, 'character_creating_combo_for_model'] = 0
+        df.at[index, 'combo_block_for_model'] = current_combo_id 
+        df.at[index, 'character_creating_combo_for_model'] = 0
 
-        if df_2.at[index, 'lower_post_internal_character_id'] == 1:
-            df_2.at[index, 'humanlabel'] = 'fox'
+        if df.at[index, 'lower_post_internal_character_id'] == 1:
+            df.at[index, 'humanlabel'] = 'fox'
         else:
-            df_2.at[index, 'humanlabel'] = 'falco'
+            df.at[index, 'humanlabel'] = 'falco'
 
 
     elif row["higher_post_action_state_id"] not in action_state_id_to_check_getting_hit: #higher port player creating combo
-        df_2.at[index, 'combo_block_for_model'] = current_combo_id 
-        df_2.at[index, 'character_creating_combo_for_model'] = 1
+        df.at[index, 'combo_block_for_model'] = current_combo_id 
+        df.at[index, 'character_creating_combo_for_model'] = 1
 
-        if df_2.at[index, 'higher_post_internal_character_id'] == 1:
-            df_2.at[index, 'humanlabel'] = 'fox'
+        if df.at[index, 'higher_post_internal_character_id'] == 1:
+            df.at[index, 'humanlabel'] = 'fox'
         else:
-            df_2.at[index, 'humanlabel'] = 'falco'
+            df.at[index, 'humanlabel'] = 'falco'
 
     if row['lower_post_action_state_id'] in death_action_state_ids:
         lower_port_post_percent = 0.0
@@ -91,37 +89,37 @@ for index, row in df_2.iterrows():
         higher_port_post_percent = 0.0
 
     if row['lower_post_percent'] > lower_port_post_percent and row['lower_post_action_state_id'] not in death_action_state_ids:
-        df_2.at[index, 'attack_state_to_hit_in_combo_for_model'] = row['higher_post_last_attack_landed']
+        df.at[index, 'attack_state_to_hit_in_combo_for_model'] = row['higher_post_last_attack_landed']
         lower_port_post_percent = row['lower_post_percent']
 
     if row['higher_post_percent'] > higher_port_post_percent and row['higher_post_action_state_id'] not in death_action_state_ids:
-        df_2.at[index, 'attack_state_to_hit_in_combo_for_model'] = row['lower_post_last_attack_landed']
+        df.at[index, 'attack_state_to_hit_in_combo_for_model'] = row['lower_post_last_attack_landed']
         higher_port_post_percent = row['higher_post_percent']
     
     if row['lower_post_l_cancel_status'] == 0:
-        df_2.at[index, 'lower_port_l_cancel_for_model'] = no_l_cancel_on_frame
+        df.at[index, 'lower_port_l_cancel_for_model'] = no_l_cancel_on_frame
     
     elif row['lower_post_l_cancel_status'] == 1:
-        df_2.at[index, 'lower_port_l_cancel_for_model'] = successful_l_cancel
+        df.at[index, 'lower_port_l_cancel_for_model'] = successful_l_cancel
     
     elif row['lower_post_l_cancel_status'] == 2:
-        df_2.at[index, 'lower_port_l_cancel_for_model'] = unsuccessful_l_cancel
+        df.at[index, 'lower_port_l_cancel_for_model'] = unsuccessful_l_cancel
 
     if row['higher_post_l_cancel_status'] == 0:
-        df_2.at[index, 'higher_port_l_cancel_for_model'] = no_l_cancel_on_frame
+        df.at[index, 'higher_port_l_cancel_for_model'] = no_l_cancel_on_frame
     
     elif row['higher_post_l_cancel_status'] == 1:
-        df_2.at[index, 'higher_port_l_cancel_for_model'] = successful_l_cancel
+        df.at[index, 'higher_port_l_cancel_for_model'] = successful_l_cancel
     
     elif row['higher_post_l_cancel_status'] == 2:
-        df_2.at[index, 'higher_port_l_cancel_for_model'] = unsuccessful_l_cancel
+        df.at[index, 'higher_port_l_cancel_for_model'] = unsuccessful_l_cancel
     
     last_frame = current_frame
 
     #if the state of the frame before was not in the check to get hit frames and it is now in death action state
     #then we want to label this as dying not in hitstun, and we want to label the combo row as 
 
-grouped = df_2.groupby('combo_block_for_model')
+grouped = df.groupby('combo_block_for_model')
 
 higher_port_previous_end_percent = 0.0
 lower_port_previous_end_percent = 0.0
@@ -150,17 +148,17 @@ for name, group in grouped:
         damage_done_to_lower = None
 
     if damage_done_to_lower == 0:
-        df_2.at[group.index[-1], 'higher_port_damage_done_with_combo'] = None
+        df.at[group.index[-1], 'higher_port_damage_done_with_combo'] = None
 
     else:
-        df_2.at[group.index[-1], 'higher_port_damage_done_with_combo'] = damage_done_to_lower
+        df.at[group.index[-1], 'higher_port_damage_done_with_combo'] = damage_done_to_lower
     
 
     if damage_done_to_higher == 0:
-        df_2.at[group.index[-1], 'lower_port_damage_done_with_combo'] = None
+        df.at[group.index[-1], 'lower_port_damage_done_with_combo'] = None
 
     else:
-        df_2.at[group.index[-1], 'lower_port_damage_done_with_combo'] = damage_done_to_higher
+        df.at[group.index[-1], 'lower_port_damage_done_with_combo'] = damage_done_to_higher
     
     # Update the previous end percent for the next iteration
     higher_port_previous_end_percent = end_percent_higher
@@ -168,48 +166,48 @@ for name, group in grouped:
 
 #if the character creating the model is 0 then we grab the postion of the higher port, if the character creating the combo is 1 then grab the postion of the lower port
 
-for index, row in df_2.iterrows():
+for index, row in df.iterrows():
 
     lower_port_percent_damage_done = row['lower_port_damage_done_with_combo']
     higher_port_percent_damage_done = row['higher_port_damage_done_with_combo']
 
     if row['lower_port_damage_done_with_combo'] == None:
-        df_2.at[index, 'lower_port_damage_done_with_combo_model_score'] = None
+        df.at[index, 'lower_port_damage_done_with_combo_model_score'] = None
     
     else:
-        df_2.at[index, 'lower_port_damage_done_with_combo_model_score'] = lower_port_percent_damage_done/100
+        df.at[index, 'lower_port_damage_done_with_combo_model_score'] = lower_port_percent_damage_done/100
 
     if row['higher_port_damage_done_with_combo'] == None:
-        df_2.at[index, 'higher_port_damage_done_with_combo_model_score'] = None
+        df.at[index, 'higher_port_damage_done_with_combo_model_score'] = None
 
     else:
-        df_2.at[index, 'higher_port_damage_done_with_combo_model_score'] = higher_port_percent_damage_done/100
+        df.at[index, 'higher_port_damage_done_with_combo_model_score'] = higher_port_percent_damage_done/100
 
 
 
 
 # Create masks to identify death state blocks
-df_2['higher_death_state_blocks'] = df_2['higher_post_action_state_id'].isin(death_action_state_ids)
-df_2['lower_death_state_blocks'] = df_2['lower_post_action_state_id'].isin(death_action_state_ids)
+df['higher_death_state_blocks'] = df['higher_post_action_state_id'].isin(death_action_state_ids)
+df['lower_death_state_blocks'] = df['lower_post_action_state_id'].isin(death_action_state_ids)
 
 # Create a cumulative sum as a block id for consecutive death states
-df_2['higher_block_death_id'] = (~df_2['higher_death_state_blocks']).cumsum()
-df_2['lower_block_death_id'] = (~df_2['lower_death_state_blocks']).cumsum()
+df['higher_block_death_id'] = (~df['higher_death_state_blocks']).cumsum()
+df['lower_block_death_id'] = (~df['lower_death_state_blocks']).cumsum()
 
 # Get the first index of each death block for higher and lower death states
-first_higher_death_indices = df_2[df_2['higher_death_state_blocks']].groupby('higher_block_death_id').head(1).index
-first_lower_death_indices = df_2[df_2['lower_death_state_blocks']].groupby('lower_block_death_id').head(1).index
+first_higher_death_indices = df[df['higher_death_state_blocks']].groupby('higher_block_death_id').head(1).index
+first_lower_death_indices = df[df['lower_death_state_blocks']].groupby('lower_block_death_id').head(1).index
 
 # Keep only the first occurrence of each block
-df_2 = df_2[(df_2.index.isin(first_higher_death_indices)) | (~df_2['higher_death_state_blocks'])]
-df_2 = df_2[(df_2.index.isin(first_lower_death_indices)) | (~df_2['lower_death_state_blocks'])]
+df = df[(df.index.isin(first_higher_death_indices)) | (~df['higher_death_state_blocks'])]
+df = df[(df.index.isin(first_lower_death_indices)) | (~df['lower_death_state_blocks'])]
 
 columns_to_drop = ['higher_death_state_blocks', 'lower_death_state_blocks', 'higher_block_death_id', 'lower_block_death_id']
 
 # Drop the columns from the DataFrame
-df_2.drop(columns=columns_to_drop, inplace=True)
+df.drop(columns=columns_to_drop, inplace=True)
 
-last_rows = df_2.groupby('combo_block_for_model').tail(1)
+last_rows = df.groupby('combo_block_for_model').tail(1)
 
 
 def update_position_score(row):
@@ -242,9 +240,9 @@ def update_position_score(row):
 last_rows = last_rows.apply(update_position_score, axis=1)
 
 
-df_2.update(last_rows)
+df.update(last_rows)
 
-html_data = df_2.to_html()
+html_data = df.to_html()
 
 # Write the HTML string to an HTML file
 with open('dataframe.html', 'w') as file:
@@ -254,7 +252,7 @@ csv_file = 'falco_vs_fox_csv_battlefield'
 
 # Append DataFrame to CSV file
 with open(csv_file, 'a', newline='', encoding='utf-8') as f:
-    df_2.to_csv(f, header=f.tell()==0, index=False)
+    df.to_csv(f, header=f.tell()==0, index=False)
 
 
 
