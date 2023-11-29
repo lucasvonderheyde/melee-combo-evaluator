0f59834835684519851924515bdd4c03ffecd738
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-11-28 20:51:18 -0500

    ready for presentation small profile updates needed

--- melee-combo-suggestor_frontend/src/Pages/ProfilePage/ProfilePage.js
+++ melee-combo-suggestor_frontend/src/Pages/ProfilePage/ProfilePage.js
@@ -1,11 +1,64 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react';
+import axios from 'axios';
 import NavBar from '../../components/NavBar/NavBar';
 import { AuthContext } from '../../AuthContext';
 import LogoutButton from '../../components/LogoutButton/LogoutButton';
-import './ProfilePage.css'
+import './ProfilePage.css';
 
 const ProfilePage = () => {
-    const { user } = useContext(AuthContext);
+    const { user, setUser } = useContext(AuthContext);
+
+    // State for editing fields
+    const [editing, setEditing] = useState(false);
+    const [newUsername, setNewUsername] = useState('');
+    const [newEmail, setNewEmail] = useState('');
+    const [favoriteCombo, setFavoriteCombo] = useState('');
+    const [mainCharacter, setMainCharacter] = useState('');
+    const [secondaryCharacter, setSecondaryCharacter] = useState('');
+    const [message, setMessage] = useState('');
+
+    // Update state when user context changes
+    useEffect(() => {
+        if (user) {
+            setNewUsername(user.username);
+            setNewEmail(user.email);
+            setFavoriteCombo(user.favorite_combo);
+            setMainCharacter(user.main_character);
+            setSecondaryCharacter(user.secondary_character);
+        }
+    }, [user]);
+
+    const handleProfileUpdate = async (e) => {
+        e.preventDefault();
+        let profileUpdated = false;
+
+        // Preparing JSON data for the account update
+        const accountData = {
+            user_id: user.id,
+            new_username: newUsername,
+            new_email: newEmail,
+            favorite_combo: favoriteCombo,
+            main_character: mainCharacter,
+            secondary_character: secondaryCharacter
+        };
+
+        // Updating account details
+        try {
+            const accountResponse = await axios.post('http://127.0.0.1:5555/update-account', accountData);
+            console.log("Response from update-account:", accountResponse.data);
+
+            profileUpdated = accountResponse.data.message;
+        } catch (error) {
+            setMessage(error.response?.data?.message || 'Profile update failed');
+            return;
+        }
+
+        // Set success message
+        if (profileUpdated) {
+            setMessage('Profile updated successfully!');
+            setEditing(false);
+        }
+    };
 
     return (
         <div>
@@ -16,11 +69,50 @@ const ProfilePage = () => {
                     <div>
                         <p><strong>Username:</strong> {user.username}</p>
                         <p><strong>Email:</strong> {user.email}</p>
+                        <p><strong>Favorite Combo:</strong> {user.favorite_combo || 'Not specified'}</p>
+                        <p><strong>Main Character:</strong> {user.main_character || 'Not specified'}</p>
+                        <p><strong>Secondary Character:</strong> {user.secondary_character || 'Not specified'}</p>
                     </div>
                 )}
+                <button onClick={() => setEditing(!editing)}>{editing ? 'Cancel Editing' : 'Edit Profile'}</button>
+                {editing && (
+                    <form onSubmit={handleProfileUpdate}>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={newUsername}
+                            onChange={(e) => setNewUsername(e.target.value)}
+                        />
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={newEmail}
+                            onChange={(e) => setNewEmail(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Favorite Combo"
+                            value={favoriteCombo}
+                            onChange={(e) => setFavoriteCombo(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Main Character"
+                            value={mainCharacter}
+                            onChange={(e) => setMainCharacter(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Secondary Character"
+                            value={secondaryCharacter}
+                            onChange={(e) => setSecondaryCharacter(e.target.value)}
+                        />
+                        <button type="submit">Update Profile</button>
+                    </form>
+                )}
+                {message && <p className="profile-message">{message}</p>}
                 <LogoutButton />
             </div>
-            
         </div>
     );
 };
